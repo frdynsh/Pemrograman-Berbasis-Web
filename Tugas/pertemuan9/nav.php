be92e95b688f339e660437f1e63108aad55e5377
Ferdi Yansah <frdynsh11@gmail.com>
2025-06-04 12:53:42 +0700

    membuat tugas pertemuan9

--- Tugas/pertemuan9/nav.php
+++ Tugas/pertemuan9/nav.php
@@ -1,7 +1,7 @@
 <!-- Menu Navigasi -->
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark  ">
        <div class="container">
-           <a class="navbar-brand" href="index.php">Toko Buku China</a>
+           <a class="navbar-brand" href="index.php">Toko Buku</a>
            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
            </button>
